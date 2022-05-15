f0f962f4ac0d3912be44d94d447c559b07775ec1
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-05-15 18:08:05 -0400

    Updated changelog and version

--- EngineLightRelit/AssemblyVersion.cs
+++ EngineLightRelit/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyFileVersion("1.6.3.2")]
- [assembly: AssemblyVersion("1.6.3.2")]
+ [assembly: AssemblyFileVersion("1.6.3.3")]
+ [assembly: AssemblyVersion("1.6.3.3")]
